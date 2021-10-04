a6d3aad41c19168920c6560c20fb43d201394a92
juliajoswig <86786448+juliajoswig@users.noreply.github.com>
2021-10-04 22:01:07 +0200

    remove supplement until ready to publish

--- Supplement/scripts/_support_functions/fn_add_alpha.R
+++
@@ -1,7 +0,0 @@
-add.alpha <- function(col, alpha){
-  if(missing(col))
-    stop("Please provide a vector of colours.")
-  apply(sapply(col, col2rgb)/255, 2, 
-        function(x) 
-          rgb(x[1], x[2], x[3], alpha=alpha))  
-}
--- Supplement/scripts/_support_functions/target_order.R
+++
@@ -1,3 +0,0 @@
-#colours used
-target_order1=c("SeLen","DispULen","SeedMass","PlantHeight","LeFMass","LeArea","ConduitDens",
-                "SSD","LeC","LeNP","LeP","LeNArea","SLA","LeN", "VesLen","Led15N","SenbU")
